076c6b548d9ce6836579d76b8005d7b02e50cc6e
pau fraces <pfraces@gmail.com>
2013-12-03 20:01:51 +0100

    improved comments. simpler yet more verbose code
    closed #41

--- src/lib/fs/tree.js
+++ src/lib/fs/tree.js
@@ -14,25 +14,42 @@ module.exports = function (deps) {
     /**
      * define the current path:
      *
-     * - root path: '/'
-     * - no root path: '/foo'
+     * - root node path: '/'
+     * - child node path: '/foo'
      *
-     * as noted, only the root path has an ending '/', however all paths
-     * have a starting '/', so we can directly append '/' and the current
-     * node name to the current path and in order to prevent from paths
-     * starting with `//` the root case must be treated as if it was ''.
+     * as noted, only the root path has a trailing slash, however all
+     * paths have a starting slash, so we can directly append a slash
+     * and the current node name to the current path and  we will get
      *
-     * The root path is defined as '/' as a feedback to the user, so we
-     * can use that path directly in the prompt and that is the reason
-     * for this hack
+     * - root node path: '//'
+     * - child node path: '/foo/bar/
      *
-     * so here we are defining a parentPath variable which will have the
-     * something like '/foo/bar/' with starting and trailing slashes
+     * the root node needs to be treated in a particular way avoiding
+     * the double slash
+     *
+     * the root path is defined as '/' as a feedback to the user, so we
+     * can use that path directly in the prompt. that is the reason for
+     * this hack
+     *
+     * so here we are defining a parentPath variable which will have
+     * something like '/foo/bar/' with starting and trailing slashes or
+     * just '/' for the root
      *
      * then we could define the node path as the result of concatenating
      * parentPath and node name
+     *
+     * in the original code, an empty string is passed as a root path
+     * and then, when the recursive process is over, the root node path
+     * is modified (outside the process) with the user friendly single
+     * slashed root path
      */
-    var parentPath = parent.path + parent.path === '/' ? '' : '/';
+    var delimiter = '/',
+        root = delimiter,
+        parentPath = parent.path;
+    
+    if (parentPath !== root) {
+      parentPath += delimiter;
+    }
 
     /**
      * Generate a valid PathHandler object recursively from the starting
